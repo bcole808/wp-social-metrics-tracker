167e7f786e8340285fc9663eb7989509fb469251
Ben Cole <cole@chapman.edu>
2014-05-27 13:58:31 -0700

    increment version number

--- social-metrics-tracker.php
+++ social-metrics-tracker.php
@@ -3,7 +3,7 @@
 Plugin Name: Social Metrics Tracker
 Plugin URI: https://github.com/ChapmanU/wp-social-metrics-tracker
 Description: Collect and display social network shares, likes, tweets, and view counts of posts.
-Version: 1.0.2
+Version: 1.2.0
 Author: Ben Cole, Chapman University
 Author URI: http://www.bencole.net
 License: GPLv2+
