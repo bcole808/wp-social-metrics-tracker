32e7f7241b664d3df24fb412d65f2157038b9b8a
Ben Cole <cole@chapman.edu>
2015-02-04 08:32:42 -0800

    Update uninstalll.php
    New meta fields added for cleanup

--- src/uninstall.php
+++ src/uninstall.php
@@ -50,6 +50,9 @@ $wpdb->query( "DELETE FROM {$wpdb->prefix}postmeta WHERE meta_key = 'social_aggr
 $wpdb->query( "DELETE FROM {$wpdb->prefix}postmeta WHERE meta_key = 'social_aggregate_score_decayed'" );
 $wpdb->query( "DELETE FROM {$wpdb->prefix}postmeta WHERE meta_key = 'social_aggregate_score_decayed_last_updated'" );
 
+// Social Metrics alternate source URLs
+$wpdb->query( "DELETE FROM {$wpdb->prefix}postmeta WHERE meta_key = 'socialcount_alt_data'" );
+
 // Remove all scheduled cron tasks
 include_once('MetricsUpdater.class.php');
 MetricsUpdater::removeAllQueuedUpdates();
