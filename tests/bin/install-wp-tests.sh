2b431938dc6e66444ef290b4373e9fc3555483bd
Ben Cole <cole@chapman.edu>
2015-06-11 16:17:10 -0700

    Browser tests for single site settings pages

--- tests/bin/install-wp-tests.sh
+++ tests/bin/install-wp-tests.sh
@@ -71,6 +71,7 @@ install_test_suite() {
 	sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
 	sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
 	sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
+	sed $ioption "s/wptests_/wp_/" wp-tests-config.php
 	sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
 }
 
---
+++ tests/test-settings.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+* @group selenium
+*/
+class TestSettings extends PHPUnit_Extensions_Selenium2TestCase {
+
+	function setUp() {
+		parent::setUp();
+
+		$this->setBrowser('firefox');
+		$this->setBrowserUrl( 'http://localhost:9001' );
+
+	}
+
+	function tearDown() {
+		parent::tearDown();
+	}
+
+	/***************************************************
+	* Make sure that each settings page can be saved and it does not overwrite other pages
+	***************************************************/
+	function test_settings_are_saved() {
+
+		// Install DB
+		shell_exec('rake test:install_single');
+		
+		$this->login();
+		$this->activate_plugin();
+
+		// CHANGES
+		$this->go_to_settings_page();
+		$this->assert_default_settings();
+		$this->change_settings_general();
+
+		$this->go_to_settings_page('API Connection Settings');
+		$this->change_settings_api();
+		
+		$this->go_to_settings_page('Advanced Domain / URL Setup');
+		$this->change_settings_domain();
+
+		// ASSERTIONS
+		$this->go_to_settings_page('General Settings');
+		$this->assert_settings_general();
+
+		$this->go_to_settings_page('API Connection Settings');
+		$this->assert_settings_api();
+
+		$this->go_to_settings_page('Advanced Domain / URL Setup');
+		$this->assert_settings_domain();
+
+	}
+
+
+
+	/***************************************************
+
+	* HELPERS
+
+	***************************************************/
+
+	function login() {
+		$this->timeouts()->implicitWait(10000);
+
+		$this->url( '/wp-admin' );
+
+		sleep(1);
+
+		$form = $this->byId( 'loginform' );
+
+		$this->byId( 'user_login' )->value( 'admin' );
+		$this->byId( 'user_pass' )->value( 'admin' );
+
+		$form->submit();
+
+	}
+
+	function activate_plugin() {
+		$this->url( '/wp-admin/plugins.php' );
+		$activate = $this->byCssSelector('#social-metrics-tracker .row-actions .activate a');
+		$activate->click();
+
+		$this->assertContains('Plugin activated', $this->byCssSelector('#message')->text());
+	}
+
+	function go_to_settings_page($section=false) {
+		$menuitem = $this->byCssSelector('#toplevel_page_social-metrics-tracker');
+		$menuitem->click();
+
+		$thing = $this->byLinkText("Configuration");
+		$thing->click();
+
+		$this->assertContains('Social Metrics Tracker Configuration', $this->title());
+
+		if ( false !== $section && 'General Settings' !== $section ) {
+			$this->byLinkText($section)->click();
+		}
+
+	}
+
+	function assert_default_settings() {
+		$this->assertTrue($this->byId('smt_options_post_types_post')->selected(), 'Default settings wrong!');
+		$this->assertTrue($this->byId('smt_options_post_types_page')->selected(), 'Default settings wrong!');
+		$this->assertFalse($this->byId('smt_options_post_types_attachment')->selected(), 'Default settings wrong!');
+	}
+
+	function change_settings_general() {
+		// Posts: ON
+		if ( !$this->byId('smt_options_post_types_post')->selected() )
+			  $this->byId('smt_options_post_types_post')->click();
+
+		// Pages: OFF
+		if (  $this->byId('smt_options_post_types_page')->selected() )
+			  $this->byId('smt_options_post_types_page')->click();
+
+		// Media: ON
+		if ( !$this->byId('smt_options_post_types_attachment')->selected() )
+			  $this->byId('smt_options_post_types_attachment')->click();
+
+		// WIDGET: OFF
+		if (  $this->byId('smt_options_display_widget')->selected() )
+			  $this->byId('smt_options_display_widget')->click();
+
+
+		$select = $this->select($this->byId('smt_options_report_visibility'));
+		$select->selectOptionByLabel('Admins (Users who can manage options)');
+
+		// save form
+		$this->byCssSelector('.button-primary')->click();
+	}
+
+	function assert_settings_general() {
+
+		$this->assertTrue($this->byId('smt_options_post_types_post')->selected(), 'Default settings wrong!');
+		$this->assertFalse($this->byId('smt_options_post_types_page')->selected(), 'Default settings wrong!');
+		$this->assertTrue($this->byId('smt_options_post_types_attachment')->selected(), 'Default settings wrong!');
+
+	}
+
+	function change_settings_api() {
+
+		$this->byCssSelector('input[value="graph"]')->click();
+
+		// EXTERNAL HTTP TEST!
+		// $this->byId('fb_app_id')->value('my-app-id');
+		// $this->byId('fb_app_secret')->value('my-app-secret');
+
+		// save form
+		$this->byCssSelector('.button-primary')->click();
+
+	}
+
+	function assert_settings_api() {
+		$this->assertFalse($this->byCssSelector('input[value="public"]')->selected());
+		$this->assertTrue($this->byCssSelector('input[value="graph"]')->selected());
+	}
+
+	function change_settings_domain() {
+
+		$select = $this->select($this->byId('url_protocol'));
+		$select->selectOptionByLabel('Check both http:// and https:// URLs');
+
+		$this->byId('rewrite_before_date')->value('2015-06-12');
+		$this->byId('rewrite_change_to')->value('http://www.my-old-url.com');
+
+		$select = $this->select($this->byId('alt_url_ttl_multiplier'));
+		$select->selectOptionByLabel('Much slower refresh rate (better server performance)');
+
+		// save form
+		$this->byCssSelector('.button-primary')->click();
+
+	}
+
+	function assert_settings_domain() {
+
+		$select = $this->select($this->byId('url_protocol'));
+		$this->assertEquals('Check both http:// and https:// URLs', $select->selectedLabel());
+    	$this->assertEquals('both', $select->selectedValue());
+
+    	$this->assertEquals('2015-06-12', $this->byId('rewrite_before_date')->value());
+    	$this->assertEquals('http://www.my-old-url.com', $this->byId('rewrite_change_to')->value());
+
+		$select = $this->select($this->byId('alt_url_ttl_multiplier'));
+		$this->assertEquals('Much slower refresh rate (better server performance)', $select->selectedLabel());
+    	$this->assertEquals('10', $select->selectedValue());
+
+	}
+
+
+
+}
